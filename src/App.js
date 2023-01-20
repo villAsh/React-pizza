0835a917e3c3769c89d68e8d0754d824a481e362
vilas chavhan <chavhanvilas02@gmail.com>
2023-01-20 16:56:02 +0530

    added cartcontext

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useSearchParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
 // import About from "./Pages/About";
 import ProductsPage from "./Pages/ProductsPage";
 import SingleProduct from "./Components/SingleProduct";
 import Cart from "./Pages/Cart";
+import { CartContext } from "./CartContext";
+
 function App() {
+  const [cart, setCart] = useState({});
+
+  useEffect(() => {
+    const cart = window.localStorage.getItem('cart');
+  }, [])
+
+  useEffect(()=>{
+    window.localStorage.setItem('cart',JSON.stringify(cart));
+  },[cart])
+
   return (
     <div className="App">
-        <Router>
+      <Router>
+        <CartContext.Provider value={{ cart,setCart }}>
           <Navbar />
-            <Routes>
-              <Route path="/" element={<Home />} exact />
-              {/* <Route path="about" element={<About />} /> */}
-              <Route path="products" element={<ProductsPage />} />
-              <Route path="product/:_id" element={<SingleProduct />} />
-              <Route path="cart" element={<Cart />} />
-            </Routes>
-        </Router>
+          <Routes>
+            <Route path="/" element={<Home />} exact />
+            {/* <Route path="about" element={<About />} /> */}
+            <Route path="products" element={<ProductsPage />} />
+            <Route path="product/:_id" element={<SingleProduct />} />
+            <Route path="cart" element={<Cart />} />
+          </Routes>
+        </CartContext.Provider>
+      </Router> 
     </div>
   );
 }
--- src/Components/SingleProduct.js
+++ src/Components/SingleProduct.js
@@ -1,10 +1,12 @@
-import { useState, useEffect,memo } from "react";
+import { useState, useEffect,memo, useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-
+import { CartContext } from "../CartContext";
 function SingleProduct(){
     const [product, setProduct] = useState({})
     const params = useParams();
     const navigate = useNavigate();
+    const { cart,setCart } = useContext(CartContext);
+    
     useEffect(()=>{
         fetch(`https://star-spark-pasta.glitch.me/api/products/${params._id}`)
         .then(response => response.json())
@@ -12,6 +14,29 @@ function SingleProduct(){
             setProduct(data)
         })
     },[params._id])
+
+    const AddToCart = (e,product) =>{
+        e.preventDefault();
+        let _cart = {...cart};
+
+        if(!_cart.items){
+            _cart.items = {};
+        }
+
+        if(_cart.items[product._id]){
+            _cart.items[product._id] += 1;
+        }else{
+            _cart.items[product._id] = 1;
+        }
+
+        if(!_cart.totalItems){
+            _cart.totalItems = 0;
+        }
+        _cart.totalItems += 1;
+
+        setCart(_cart);
+
+    }
     return(
         <div className="container mx-auto px-6">
             <button className="text-xl font-bold mt-8" onClick={() => navigate(-1)}>Back</button>
@@ -21,7 +46,7 @@ function SingleProduct(){
                     <h1 className="text-xl font-bold">{product.name}</h1>
                     <h3 className="font-semibold">{product.size}</h3>
                     <h3 className="text-lg font-semibold">&#8377; {product.price}</h3>
-                    <button className="font-bold bg-yellow-500 px-8 py-2 rounded-full">Add To Cart</button>
+                    <button onClick={(e) => AddToCart(e,product)} className="font-bold bg-yellow-500 px-8 py-2 rounded-full">Add To Cart</button>
                 </div>
             </div>
         </div>
